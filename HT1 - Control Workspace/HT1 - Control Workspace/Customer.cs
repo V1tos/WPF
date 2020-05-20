7516113745cd1f13126c420c12e2f6ac9a2a8682
V1tos <victor.palamarchuk@gmail.com>
2020-05-20 18:02:54 +0300

    Hallo

---
+++ HT1 - Control Workspace/HT1 - Control Workspace/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT1___Control_Workspace
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string ProfType { get; set; }
+
+        public Customer(string name, string surname, string eMail, string profType)
+        {
+            Name = name;
+            Surname = surname;
+            Email = eMail;
+            ProfType = profType;   
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} - {Email} - {ProfType}";
+        }
+
+    }
+}
---
+++ HT1 - Control Workspace/HT1 - Control Workspace/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HT1___Control_Workspace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Customer customer;
+        public MainWindow()
+        {
+            InitializeComponent();        
+            cbType.Items.Add("Low");
+            cbType.Items.Add("Middle");
+            cbType.Items.Add("High");
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            customer = new Customer(tbName.Text, tbSurname.Text, tbEmail.Text, cbType.SelectedValue.ToString());
+            lbCustomers.Items.Add(customer);
+        }
+    }
+}
