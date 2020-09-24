645518fe1f1c7e497645671866c867439280e1da
V1tos <victor.palamarchuk@gmail.com>
2020-09-25 02:01:33 +0300

    .

---
+++ HT 8 - Regisrtation Form/HT 8 - Regisrtation Form/ViewModel/Commands/RegistrationCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HT_8___Regisrtation_Form.ViewModel.Commands
+{
+    public class RegistrationCommand
+    {
+        public UserVM VM { get; set; }
+
+        public RegistrationCommand(UserVM vm)
+        {
+            VM = vm;
+        }
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VM.AddUsers();
+        }
+    }
+}
---
+++ HT 8 - Regisrtation Form/HT 8 - Regisrtation Form/ViewModel/UserVM.cs
@@ -0,0 +1,65 @@
+﻿using HT_8___Regisrtation_Form.Model;
+using HT_8___Regisrtation_Form.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT_8___Regisrtation_Form.ViewModel
+{
+    public class UserVM : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<User> Users { get; set; } = new ObservableCollection<User>();
+
+        public RegistrationCommand RegistrationCommand;
+
+        string answer;
+        public string Answer
+        {
+            get => answer;
+            set
+            {
+                answer = value;
+                OnNotify();
+            }
+        }
+
+
+        public User user;
+        public User User
+        {
+            get => user;
+            set
+            {
+                user = value;
+                OnNotify();
+            }
+        }
+
+        public UserVM()
+        {
+            User = new User();
+            RegistrationCommand = new RegistrationCommand(this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnNotify([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        public void AddUsers()
+        {
+
+            Users.Add(new User { FirstName = User.FirstName, LastName = User.LastName, Email = User.Email, Phone = User.Phone, Birthday = User.Birthday, Gender = User.Gender, Password = User.Password });
+            Answer = "We have this User !!!!";
+        }
+
+    }
+}
